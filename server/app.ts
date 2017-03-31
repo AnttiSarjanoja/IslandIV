38d5e361226bf87ad19ed1293c32d0046215e492
Antti Sarjanoja <antesarj@gmail.com>
2017-03-31 21:06:03 +0300

    Backend load settings too + Game class started

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,15 @@
 import * as path from 'path';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as fs from 'fs';
 
+import Game from "./game";
 import * as db from "./db";
 
 // Creates and configures an ExpressJS web server.
 class App {
 	public express: express.Application;
-	public Games: IGame[] = []; // TODO: Backend class for game stuff
+	public Games: Game[] = []; // TODO: Backend class for game stuff
 
 	//Run configuration methods on the Express instance.
 	constructor() {
@@ -20,17 +22,46 @@ class App {
 				if (games !== undefined && games.length > 0) {
 					console.log("Loaded following games from db:");
 					console.log(games.map((game: IGame) => { return game.name + " turn: " + game.turn; }));
-					this.Games = games;
+					this.initGames(games);
 				}
 				else console.log("Got some random *dung*");
 			});
 		});
 	}
 
+	private initGames(games: IGame[]) {
+		for(let game of games) {
+			try {
+				let newGame: Game = new Game(game,
+					JSON.parse(fs.readFileSync(__dirname + '/public/' + game.provinceFile, 'utf8')),
+					JSON.parse(fs.readFileSync(__dirname + '/public/' + game.settingsFile, 'utf8'))
+				);
+				this.Games.push(newGame);
+			}
+			catch (e) {
+				console.log("Error initing game: '" + game.name + "'");
+				console.log("  Reason: " + e);
+			}
+		}
+	}
+
   // Configure Express middleware.
 	private middleware(): void {
 		this.express.use(bodyParser.json());
 		this.express.use(bodyParser.urlencoded({ extended: false }));
+	}
+
+	// TODO: Any other way of stripping non-IGame stuff away from 'any' ?
+	private filterGameData(game: any): IGame {
+		return {
+			name: game.name,
+			players: game.players,
+			messages: game.messages,
+			turn: game.turn,
+			settingsFile: game.settingsFile,
+			provinceFile: game.provinceFile,
+			religions: game.religions
+		}
 	}
 
 	// Configure API endpoints.
@@ -40,11 +71,11 @@ class App {
 			if (this.Games.length > 0) {
 				console.log("Sending actual gamedata");
 				// TODO: Remove _id *dung* from sent object
-				res.status(200).json(this.Games[0]); // TODO: Get the requested game, now there is only one
+				res.status(200).json(this.filterGameData(this.Games[0])); // TODO: Get the requested game, now there is only one
 			}
 			else {
 				console.log("Sending dummy gamedata");
-				res.status(200).json(db.createGame()); // TODO: Status does not work here :(
+				res.status(200).json(this.filterGameData(db.createGame())); // TODO: Status does not work here :(
 			} // Send dummydata when not using DB
 		});
 		this.express.post('/orders', (req, res) => {
---
+++ server/game.ts
@@ -0,0 +1,39 @@
+/// <reference path="../common/interfaces.ts" />
+/// <reference path="../common/settings.ts" />
+
+// TODO: Now this is kind of copypaste of client-side Game atm. so is there any way to make this work with non-module solution?
+export default class Game implements IGame {
+	readonly name: string;
+	readonly players: IPlayer[] = [];
+	readonly messages: IMessage[] = [];
+	readonly turn: number;
+	readonly settingsFile: string = "";
+	readonly provinceFile: string = "";
+	readonly religions: IReligion[] = [];
+
+	readonly ProvinceSettings: ProvinceSettings;
+	readonly GameSettings: GameSettings;
+
+	constructor(data: IGame, provinceSettings: ProvinceSettings, gameSettings: GameSettings) {
+		this.name = data.name;
+		this.turn = data.turn;
+		this.settingsFile = data.settingsFile;
+		this.provinceFile = data.provinceFile;
+
+		// These work now :>
+		this.ProvinceSettings = provinceSettings;
+		this.GameSettings = gameSettings;
+
+		// TODO: Classes for these
+		this.players = data.players;
+		this.messages = data.messages;
+		this.religions = data.religions;
+
+		/*
+		for (var playerData of data.players) {
+			this.players.push(new Player(playerData));	
+		}
+		Game.CurrentPlayer = this.players[0]; */
+	}
+}
+
